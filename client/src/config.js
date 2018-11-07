e95ce5565cb91418be856f390713136bd2ce2b44
Jak Hall <jakhall@hotmail.co.uk>
2018-11-07 22:36:19 +0000

    init upload

---
+++ client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "pma-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://h4fq2mq02e.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_5714IGrlU",
+    APP_CLIENT_ID: "6j3sb4nlshck3i2icdh4aaaofm",
+    IDENTITY_POOL_ID: "eu-west-2:e40ebe45-9e02-4a71-9a5b-83a2f4453db7"
+  }
+};
---
+++ client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>PMA</h1>
+          <p>A simple project management app</p>
+        </div>
+      </div>
+    );
+  }
+}
