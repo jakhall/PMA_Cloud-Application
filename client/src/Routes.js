04f2943c97f0c2e39b28556c5e0cf98b4a2dfce2
Jak Hall <jakhall@hotmail.co.uk>
2018-11-08 01:53:10 +0000

    Login System

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,10 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
 
-export default () =>
+export default ({ childProps }) =>
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" exact component={Login} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
