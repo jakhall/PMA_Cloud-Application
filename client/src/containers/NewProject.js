774183ae94031d200cad104c9f5a046584902733
Jak Hall <jakhall@hotmail.co.uk>
2018-11-09 21:27:26 +0000

    init 'project adding'

---
+++ client/src/containers/NewProject.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import config from "../config";
+import "./NewProject.css";
+import { API } from "aws-amplify";
+
+export default class NewProject extends Component {
+  constructor(props) {
+    super(props);
+
+    this.file = null;
+
+    this.state = {
+      isLoading: null,
+      content: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.content.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleFileChange = event => {
+    this.file = event.target.files[0];
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ isLoading: true });
+
+    try {
+      await this.createProject({
+        content: this.state.content
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+
+
+  createProject(project) {
+    return API.post("projects", "/projects", {
+      body: project
+    });
+  }
+
+  render() {
+    return (
+      <div className="NewProject">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="content">
+            <FormControl
+              onChange={this.handleChange}
+              value={this.state.content}
+              componentClass="textarea"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsStyle="primary"
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Create"
+            loadingText="Creating…"
+          />
+        </form>
+      </div>
+    );
+  }
+}
